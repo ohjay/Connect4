cb870066df93e539d07d743e57fc39029d5174fe
Owen Jow <owenjow@berkeley.edu>
2015-06-10 00:38:17 -0700

    Created Connect4 template file

---
+++ Connect4/Connect4.java
@@ -0,0 +1,22 @@
+package Connect4;
+
+/**
+ * The classic game of Connect Four, enhanced with several modern modifications.
+ * Built initially as a demonstration to a friend that the game is solved.
+ * Modes of gameplay include:
+ * - Easy mode (in which the computer will make mistakes)
+ * - Solved mode (in which the computer will play perfectly)
+ * - Two-player mode: single-computer (in which two humans play on one computer)
+ * - Two-player mode: multiple-computer (in which humans play on different computers)
+ * - Warfare mode (in which 3 or 4 players can play at the same time)
+ * - Four by Two (in which players can place two at a time)
+ * - Removal (in which players can opt to remove a piece as their turn, w/ up to 5 removals total)
+ * and more! pending sufficient inspiration
+ * @author Owen Jow
+ */
+public class Connect4 {
+    
+    public static void main(String[] args) {
+        /* To do: insert main method logic here */
+    }
+}
